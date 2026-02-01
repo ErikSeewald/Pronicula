42bcc7eb878fef5d441228eae1e8cedb66893543
ErikSeewald <erik.seewald.g@gmail.com>
2026-02-01 19:06:26 +0100

    better hamster to wheel transition

--- scripts/demos/hamsters/hamsters.gd
+++ scripts/demos/hamsters/hamsters.gd
@@ -2,6 +2,9 @@ extends ColorRect
 
 var time := 0.0
 
+func _ready() -> void:
+	time = material.get_shader_parameter("time")
+
 func _process(delta: float) -> void:
 	time += delta * 3.0
 	material.set_shader_parameter("time", time)
