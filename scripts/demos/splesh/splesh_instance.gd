9d1edb497f4c4834eb2f4768297c705d461f10cf
ErikSeewald <erik.seewald.g@gmail.com>
2026-03-29 11:14:35 +0200

    splesh demo and refactor

--- scripts/demos/splesh/splesh_instance.gd
+++ scripts/demos/splesh/splesh_instance.gd
@@ -2,19 +2,17 @@ extends MeshInstance2D
 
 @onready var mat: ShaderMaterial = self.material
 
-@export var num_quads: int = 100
-var t: float = 0.0
+@export var num_quads: int = 500000
+var time: float = 0.0
 
 func _ready() -> void:
 	var splesh: ArrayMesh = SpleshGenerator.generate_splesh(num_quads, SpleshGenerator.CullMode.NEVER_CULL)
-	var err = ResourceSaver.save(splesh, "res://splesh_100.tres")
 	self.mesh = splesh
-	self.scale = Vector2(20, 20)
 	mat.set_shader_parameter("num_quads", num_quads)
 	
-func set_time(time: float):
+func _process(delta: float): 
+	time += delta
 	mat.set_shader_parameter("time", time)
 	
-func _process(delta: float): 
-	t += delta
-	set_time(t)
+	# To compare a mouse pos to a vertex pos in the shader, send this to it
+	# self.to_local(get_global_mouse_position())
---
+++ scripts/menu/menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func launch_hamsters():
+	get_tree().change_scene_to_file("res://scenes/demos/Hamsters.tscn")
+	
+func launch_splesh_demo():
+	get_tree().change_scene_to_file("res://scenes/demos/splesh.tscn")
+	
+func launch_switching_tiles():
+	get_tree().change_scene_to_file("res://scenes/demos/SwitchingTiles.tscn")
