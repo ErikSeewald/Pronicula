0d066ea125fe5a344f3ec6d379d7d53a44cb3100
ErikSeewald <erik.seewald.g@gmail.com>
2026-04-04 14:55:33 +0200

    begin working on trains

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -2,9 +2,12 @@ extends Control
 
 func launch_hamsters():
 	get_tree().change_scene_to_file("res://scenes/demos/Hamsters.tscn")
-	
+
+func launch_trains():
+	get_tree().change_scene_to_file("res://scenes/demos/Trains.tscn")
+
 func launch_splesh_demo():
 	get_tree().change_scene_to_file("res://scenes/demos/splesh.tscn")
-	
+
 func launch_switching_tiles():
 	get_tree().change_scene_to_file("res://scenes/demos/SwitchingTiles.tscn")
