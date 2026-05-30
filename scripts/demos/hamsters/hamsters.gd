8a17f961f9c6745cbd402b712fa49db10bcf2e96
ErikSeewald <erik.seewald.g@gmail.com>
2026-05-30 09:52:03 +0200

    add hamster time speed variable

--- scripts/demos/hamsters/hamsters.gd
+++ scripts/demos/hamsters/hamsters.gd
@@ -1,13 +1,17 @@
 extends ColorRect
 
 var time := 0.0
+var time_speed := 1.0
 
 func _ready() -> void:
 	time = material.get_shader_parameter("time")
 
 func _process(delta: float) -> void:
-	time += delta
+	time += delta * time_speed
 	material.set_shader_parameter("time", time)
 	
-func on_change(param_value, param_name):
+func on_change(param_value, param_name) -> void:
 	material.set_shader_parameter(param_name, param_value)
+	
+func on_set_time_speed(value: float) -> void:
+	time_speed = value
