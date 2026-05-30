6553f10de6382f9568ec7719c7e757e638cdf897
ErikSeewald <erik.seewald.g@gmail.com>
2026-05-30 09:52:39 +0200

    more splesh work

---
+++ scripts/demos/trains/trains_splesh.gd
@@ -0,0 +1,15 @@
+extends MeshInstance2D
+
+@onready var mat: ShaderMaterial = self.material
+
+@export var num_quads: int = 100
+var time: float = 0.0
+
+func _ready() -> void:
+	var splesh: ArrayMesh = SpleshGenerator.generate_splesh(num_quads, SpleshGenerator.CullMode.NEVER_CULL)
+	self.mesh = splesh
+	mat.set_shader_parameter("num_quads", num_quads)
+	
+func _process(delta: float): 
+	time += delta
+	mat.set_shader_parameter("time", time)
